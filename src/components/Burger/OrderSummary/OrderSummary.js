6da910fb540fa308747f4f72ea793073ee455437
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-10-14 14:53:57 -0700

    Restructred components

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,30 +2,28 @@ import React from 'react';
 
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
+function OrderSummary(props) {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(ingredientKey => {
+            return <li key={ingredientKey}>
+                <span style={{textTransform: 'capitalize'}}>{ingredientKey}</span>
+                : {props.ingredients[ingredientKey]}
+            </li>
+        });
+    return (
+        <>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients: </p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
+            <p>Continue with checkout?</p>
+            <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
+        </>
+    );
+}
 
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(ingredientKey => {
-      return <li key={ingredientKey}>
-        <span style={{textTransform: 'capitalize'}}>{ingredientKey}</span>
-        : {props.ingredients[ingredientKey]}
-        </li>
-    });
+export default OrderSummary;
 
-
-  return (
-    <>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients: </p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-      <p>Continue with checkout?</p>
-      <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
-      <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-    </>
-  );
-};
-
-export default orderSummary;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-import Layout from './containers/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Layout from '../Layout/Layout';
+import BurgerBuilder from '../BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
