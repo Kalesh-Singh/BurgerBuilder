4e5a0d9a5fe1567b216e28429d15aa5f9e5a484e
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-10-18 13:26:33 -0700

    Outputting the fetched orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,12 +1,56 @@
 import React, {Component} from 'react';
 import Order from "../../components/Order/Order";
+import fire from "../../fire";
 
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    };
+
+    componentDidMount() {
+        this.readOrders();
+    }
+
+    readOrders = () => {
+        fire.database().ref('orders').once('value')
+            .then(snapshot => {
+                console.log(snapshot.val());
+                const fetchedOrders = [];
+                for (let key in snapshot.val()) {
+                    fetchedOrders.push({
+                        ...snapshot.val()[key],
+                        id: key
+                    });
+                }
+                setTimeout(() => {
+                    this.setState({
+                        orders: fetchedOrders,
+                        loading: false
+                    });
+                }, 1000);
+            }).catch(error => {
+            console.log(error.code, error.message);
+            setTimeout(() => {
+                this.setState({
+                    error: error,
+                    loading: false
+                });
+            }, 1000);
+        });
+    };
+
     render() {
         return (
             <div>
-                <Order/>
-                <Order/>
+                {this.state.orders.map(order => (
+                    <Order
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price}
+                    />
+                ))}
             </div>
         );
     }
